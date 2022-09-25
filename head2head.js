cb59cc5237bb227ff11635a2de370ac2560332e3
TwilCynder <twilcynder@gmail.com>
2022-09-25 23:53:11 +0200

    head to head between 2 players

--- head2head.js
+++ head2head.js
@@ -70,41 +70,56 @@ class Player {
     }
 }
 
+class Head2Head extends Array {
+    constructor(p1, p2){
+        super()
+        this[0] = {player : p1, score: 0};
+        this[1] = {player : p2, score: 0};
+    }
+
+    addResult(player){
+        this[player].score++;
+    }
+
+    toString(){
+        return "" + this[0].player.name + " " + this[0].score + " - " + this[1].score + " " + this[1].player.name;
+    }
+}
+
+function getHead2Head(p1Sets, p1, p2){
+    let result = new Head2Head(p1, p2);
+
+    p1Sets.forEach(set => {
+        let setP1 = set.slots[0].entrant.participants;
+        if (setP1.length > 1) {
+            //console.log("[Ignoring non-1v1 set]");
+            return;
+        }
+        setP1 = setP1[0].player;
+    
+        let p1PositionInSet = (p1.id == setP1.id) ? 0 : 1;
+        let otherPlayer = set.slots[1 - p1PositionInSet].entrant.participants[0].player;
+    
+        if (otherPlayer.id == p2.id){
+            //match found
+            let p1result = set.slots[p1PositionInSet].standing.placement
+            result.addResult(p1result == 1 ? 0 : 1)
+            console.log("Winner : " + ((p1result == 1) ? p1.name : p2.name))
+        }
+    });
+
+    return result
+}
+
 if (process.argv.length < 4 ){
     console.log("Need two arguments");
     process.exit()
 }
 
-let p1Slug = process.argv[2];
-let p2Slug = process.argv[3];
+let players = [new Player(process.argv[2]), new Player(process.argv[3])];
 
-let players = new Player(p1Slug);
+await Promise.all(players.map(async (p) => await p.loadPlayer()))
 
-let p1 = 
-let p2 = new Player(p2Slug);
+let h2h = getHead2Head(await getSetsFromPlayer(players[0].id), players[0], players[1])
 
-await p1.loadPlayer()
-await p2.loadPlayer()
-
-console.log(p1.id)
-let p1Sets = await getSetsFromPlayer(p1.id)
-
-p1Sets.forEach(set => {
-    let setP1 = set.slots[0].entrant.participants;
-    if (setP1.length > 1) {
-        //console.log("[Ignoring non-1v1 set]");
-        return;
-    }
-    setP1 = setP1[0].player;
-
-    let p1PositionInSet = (p1.id == setP1.id) ? 0 : 1;
-    let otherPlayer = set.slots[1 - p1PositionInSet].entrant.participants[0].player;
-
-    if (otherPlayer.id == p2.id){
-        //match found
-        console.log(set.displayScore)
-        let p1result = set.slots[p1PositionInSet].standing.placement
-        if (p1result == 1)
-    }
-
-});
+console.log(h2h)
