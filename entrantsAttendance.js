94abac6ae8606c37388c637bca302187cf8aa63c
Nillan <nillan.eldaar@lilo.org>
2023-05-31 18:05:40 +0200

    entrants attendance

--- entrantsAttendance.js
+++ entrantsAttendance.js
@@ -7,25 +7,41 @@ if (process.argv.length < 3 ){
     process.exit()
 }
 
-let lsit = [];
+let list = [];
 switch (process.argv[2]){
-    case '-f':
+    case "-f":
         console.log("-f is not suported yet sorryyyyy");
         process.exit(1);
-    case '-s':
+    case "-s":
         {
             if (process.argv.length < 3 ){
                 console.log("Usage : " + process.argv[0] + " " + process.argv[1] + " {-f listFilename | -s template min max | slugs ...} ");
                 process.exit()
             }
-            let template = process.argv[4];
-            let min = parseInt(process.argv[5]);
-            let max = parseInt(process.argv[6]);
-
+            let template = process.argv[3];
+            let min = parseInt(process.argv[4]);
+            let max = parseInt(process.argv[5]);
+            console.log(template, min, max)
             for (let i = min; i <= max; i++){
-                
+                list.push(template.replace("%", i));
             }
+            list = list.concat(process.argv.slice(6));
         }
+        break;
+    default:
+        list = process.argv.slice(2);
 }
 
-getAttendanceOverLeague(client, ["tournament/stock-o-clock-43/event/1v1-ultimate", "tournament/stock-o-clock-42/event/1v1-ultimate"]);
+let attendance = await getAttendanceOverLeague(client, list);
+let entrantsList = []
+
+for (let entrant in attendance) {
+    entrantsList.push({name: entrant, attendance: attendance[entrant]});
+}
+entrantsList.sort((a, b) => a.attendance - b.attendance);
+
+for (let e of entrantsList){
+    console.log(e);
+}
+
+console.log(entrantsList.length)
