972cd96b3d8b84fbf3fddb23a05f59aed8cff9c6
Nillan <nillan.eldaar@lilo.org>
2023-06-01 13:49:30 +0200

    entrants Attendance

--- entrantsAttendance.js
+++ entrantsAttendance.js
@@ -1,36 +1,46 @@
+import { createWriteStream } from "fs";
 import { client } from "./lib/common.js";
-import { getAttendanceOverLeague, updateEntrantsAttendanceFromSlug } from "./lib/getEntrants.js";
-
+import { getAttendanceOverLeague } from "./lib/getEntrants.js";
 
 if (process.argv.length < 3 ){
-    console.log("Usage : " + process.argv[0] + " " + process.argv[1] + " {-f listFilename | -s template min max | slugs ...} ");
+    console.log("Usage : " + process.argv[0] + " " + process.argv[1] + " [-m min_attendance] {-f listFilename | -s template min max | slugs ...} ");
     process.exit()
 }
 
+let argIndex = 2;
 let list = [];
-switch (process.argv[2]){
-    case "-f":
-        console.log("-f is not suported yet sorryyyyy");
-        process.exit(1);
-    case "-s":
-        {
-            if (process.argv.length < 3 ){
-                console.log("Usage : " + process.argv[0] + " " + process.argv[1] + " {-f listFilename | -s template min max | slugs ...} ");
-                process.exit()
+let printList = false;
+
+while(argIndex < process.argv.length){
+    switch (process.argv[argIndex]){
+        case "-f":
+            console.log("-f is not suported yet sorryyyyy");
+            process.exit(1);
+        case "-s":
+            {
+                if (process.argv.length < 3 ){
+                    console.log("Usage : " + process.argv[0] + " " + process.argv[1] + " {-f listFilename | -s template min max | slugs ...} ");
+                    process.exit()
+                }
+                let template = process.argv[argIndex + 1];
+                let min = parseInt(process.argv[argIndex + 2]);
+                let max = parseInt(process.argv[argIndex + 3]);
+                console.log(template, min, max)
+                for (let i = min; i <= max; i++){
+                    list.push(template.replace("%", i));
+                }
+                argIndex += 3;
             }
-            let template = process.argv[3];
-            let min = parseInt(process.argv[4]);
-            let max = parseInt(process.argv[5]);
-            console.log(template, min, max)
-            for (let i = min; i <= max; i++){
-                list.push(template.replace("%", i));
-            }
-            list = list.concat(process.argv.slice(6));
-        }
-        break;
-    default:
-        list = process.argv.slice(2);
+            break;
+        case "-l":  
+            printList = true;
+        default:
+            list.push(process.argv[argIndex]);
+    }
+
+    argIndex++;
 }
+
 
 let attendance = await getAttendanceOverLeague(client, list);
 let entrantsList = []
@@ -40,8 +50,28 @@ for (let entrant in attendance) {
 }
 entrantsList.sort((a, b) => a.attendance - b.attendance);
 
+let pools = {};
+let t = 1;
+let count = 0;
 for (let e of entrantsList){
-    console.log(e);
+    if (e.attendance > t){
+        if (count > 0) pools[t] = count;
+        count = 0;
+        t = e.attendance;
+    }
+    count++;
+    console.log(e.name, e.attendance);
+}
+pools[t] = count;
+
+let file = createWriteStream("out.txt", {encoding: "utf-8"});
+
+let cumulative = 0;
+for (let i = list.length; i > 0; i--){
+    if (!pools[i]) continue;
+    cumulative += pools[i];
+    console.log(i, "tournaments :", pools[i], '\t', "total :", cumulative);
+    file.write(i + '\t' + cumulative + '\t' + pools[i] + '\n');
 }
 
 console.log(entrantsList.length)
