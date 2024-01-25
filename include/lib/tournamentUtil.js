dc791012a305ee12dfbb5132f28a56c6fa78be3f
TwilCynder <twilcynder@gmail.com>
2024-01-26 00:51:27 +0100

    upset contest

---
+++ include/lib/tournamentUtil.js
@@ -0,0 +1,44 @@
+const placements = [1, 2, 3, 4, 6, 8, 12, 16, 24, 32, 64, 96, 128, 192, 256];
+
+export function getDoubleEliminationPlacementTier(placement){
+    for (let tier of placements){
+        if (placement <= tier) return tier;
+    }
+}
+
+export function getDoubleEliminationUpsetFactorFromSeeds(winnerSeed, loserSeed){
+    if (winnerSeed < loserSeed) return 0;
+
+    console.log("NON NULL SPR");
+
+    let winnerExpectedTierID, loserExpectedTierID;
+
+    for (let i = 0; i < placements.length; i++){
+        if (winnerSeed <= placements[i]) {winnerExpectedTierID = i; break;}
+    }
+
+    for (let i = 0; i < placements.length; i++){
+        if (loserSeed <= placements[i]) {
+            loserExpectedTierID = i;
+            break;
+        } 
+    }
+
+    if (!winnerExpectedTierID || !loserExpectedTierID) return undefined;
+
+    return (winnerExpectedTierID - loserExpectedTierID);
+}
+
+export function getDoubleEliminationUpsetFactorFromSet(set){
+    let score1 = set.slots[0].standing.stats.score.value;
+    let score2 = set.slots[1].standing.stats.score.value;
+    let seed1 = set.slots[0].entrant.seeds.at(-1).seedNum;
+    let seed2 = set.slots[1].entrant.seeds.at(-1).seedNum;
+
+    if (score1 < 0 || score2 < 0) return [0, 0];
+
+    console.log(set.slots[0].entrant.name, set.slots[1].entrant.name);
+    console.log(score1, score2, seed1, seed2);
+
+    return (score1 > score2) ? [getDoubleEliminationUpsetFactorFromSeeds(seed1, seed2), 0] : [getDoubleEliminationUpsetFactorFromSeeds(seed2, seed1), 1];
+}
--- upsetContest.js
+++ upsetContest.js
@@ -5,6 +5,7 @@ import { OutputModeParser, parseArguments } from "@twilcynder/goombalib-js";
 
 import { client } from "./include/lib/common.js";
 import { StartGGDelayQueryLimiter } from "./include/lib/queryLimiter.js";
+import { getDoubleEliminationUpsetFactorFromSet } from "./include/lib/tournamentUtil.js";
 
 let [outputMode, slugs] = parseArguments(process.argv.slice(2), 
     new OutputModeParser("log", "casseur2bracket"),
@@ -14,6 +15,108 @@ let [outputMode, slugs] = parseArguments(process.argv.slice(2),
 let limiter = new StartGGDelayQueryLimiter();
 let data = await getEventsSetsBasic(client, slugs, limiter);
 
-limiter.stop();
+let players = {}
+function addUpset(user, value, name, opponentName){
+    if (!user) return;
+    console.log("------- ADD --------")
+    console.log(name, opponentName, user.id);
+    let id = user.id;
+    if (players[id]){
+        players[id].upsets.push({opponent: opponentName, spr: value});
+        players[id].sets++;
+    } else {
+        players[id] = {name, upsets : [{opponent: opponentName, spr: value}], sets: 1};
+    }
+}   
 
-console.log(data);
+function addSet(user, name){
+    if (!user) return;
+    let id = user.id;
+    if (players[id]){
+        players[id].sets++;
+    } else {
+        players[id] = {name, upsets: [], sets: 1};
+    }
+}
+
+console.log("Data fetched, ${data.length} sets");
+
+for (let set of data){
+    let [spr, winner] = getDoubleEliminationUpsetFactorFromSet(set);
+
+    let winnerEntrant = set.slots[winner].entrant;
+    let loserEntrant = set.slots[1 - winner].entrant;
+
+    if (spr > 0){
+        console.log("NON NULL SPR", spr)
+        console.log(set.slots[winner].entrant.name, set.slots[1 - winner].entrant.name);
+        addUpset(winnerEntrant.participants[0].player.user, spr, winnerEntrant.name, loserEntrant.name);
+        addUpset(loserEntrant.participants[0].player.user, -spr, loserEntrant.name, winnerEntrant.name);
+    } else {
+        addSet(winnerEntrant.participants[0].player.user, winnerEntrant.name);
+        addSet(loserEntrant.participants[0].player.user, loserEntrant.name);
+    }
+}   
+
+players = Object.entries(players).map( ([id, player]) => {
+    let sum = 0;
+    let sumPos = 0;
+    for (let set of player.upsets){
+        sum += Math.abs(set.spr);
+        if (set.spr > 0) sumPos += set.spr;
+    }
+    player.total = sum;
+    player.totalPos = sumPos;
+    player.average = sum / player.sets;
+    player.averagePos = sumPos / player.sets;
+    return player;
+});
+
+console.log("===== TOTAL VALUE =====")
+players.sort((a, b) => a.total - b.total)
+for (let player of players.slice(-3)){
+    console.log("-------------");
+    console.log(player.name);
+    //console.log(player.upsets);
+    for (let set of player.upsets){
+        console.log(set.opponent, set.spr);
+    }
+    console.log(player.total);
+}
+
+console.log("===== AVERAGE =====")
+players.sort((a, b) => b.average - a.average);
+let count = 0;
+for (let player of players){
+    if (player.sets < 10) continue;
+    count++;
+    console.log(player.name, player.average, player.upsets.length, player.sets);
+
+    if (count == 5) break;
+}
+
+console.log("===== TOTAL VALUE POSITIVE =====")
+players.sort((a, b) => a.totalPos - b.totalPos)
+for (let player of players.slice(-3)){
+    console.log("-------------");
+    console.log(player.name);
+    //console.log(player.upsets);
+    for (let set of player.upsets){
+        if (set.spr > 0)
+        console.log(set.opponent, set.spr);
+    }
+    console.log(player.totalPos);
+}
+
+console.log("===== AVERAGE POSITIVE =====")
+players.sort((a, b) => b.averagePos - a.averagePos)
+count = 0;
+for (let player of players){
+    if (player.sets < 10) continue;
+    count++;
+    console.log(player.name, player.averagePos, player.upsets.length, player.sets);
+
+    if (count == 5) break;
+}
+
+limiter.stop();
