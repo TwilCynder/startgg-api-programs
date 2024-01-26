2bbd80bd5ac6ce1ce21c36d69674de5eaf153f85
TwilCynder <twilcynder@gmail.com>
2024-01-26 02:04:36 +0100

    downloading sets locally

---
+++ downloadSetsFromEvents.js
@@ -0,0 +1,21 @@
+import { getEventsSetsBasic } from "./include/getEventsSets.js";
+
+import { EventListParser } from "./include/lib/computeEventList.js";
+import { SingleOptionParser, parseArguments } from "@twilcynder/goombalib-js"; 
+
+import { client } from "./include/lib/common.js";
+import { StartGGDelayQueryLimiter } from "./include/lib/queryLimiter.js";
+
+import fs from 'fs';
+
+let [output, slugs] = parseArguments(process.argv.slice(2), 
+    new SingleOptionParser("-o"),
+    new EventListParser()
+)
+
+let limiter = new StartGGDelayQueryLimiter();
+let data = await getEventsSetsBasic(client, slugs, limiter);
+
+let filename = "./out/" + output;
+let file = fs.createWriteStream(filename, {encoding: "utf-8"});
+file.write(JSON.stringify(data));
