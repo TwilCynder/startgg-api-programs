bcf9d2e3569e261db4fb91579bddb97764bfe489
TwilCynder <twilcynder@gmail.com>
2024-06-11 19:38:34 +0200

    uniqueEntreants.js formatted output

--- uniqueEntrants.js
+++ uniqueEntrants.js
@@ -1,10 +1,11 @@
 import { ArgumentsManager, parseArguments } from "@twilcynder/arguments-parser";
 import { EventListParser } from "./include/lib/computeEventList.js";
 import { getUniqueUsersOverLeague } from "./include/getEntrants.js";
-import { client } from "./include/lib/common.js";
+import { client } from "./include/lib/client.js";
 import { StartGGDelayQueryLimiter } from "./include/lib/queryLimiter.js";
+import fs from "fs"
 
-let {list, format, outputfile, logdata} = new ArgumentsManager()
+let {list, outputFormat, outputfile, logdata} = new ArgumentsManager()
     .addCustomParser(new EventListParser, "list")
     .addOption("--format", {
         dest: "outputFormat",
@@ -26,6 +27,26 @@ let limiter = new StartGGDelayQueryLimiter;
 let users = await getUniqueUsersOverLeague(client, list, limiter);
 limiter.stop();
 
-for (let user of users){
-    console.log(user.id, user.player.gamerTag);
+if (logdata){
+    for (let user of users){
+        console.log(user.id, user.player.gamerTag);
+    }
+}
+
+let resultString = ""
+if (outputFormat == "csv"){
+    for (let user of users){
+        resultString += user.id + "\t" + user.player.gamerTag + "\n";
+    }
+} else {
+    resultString = JSON.stringify(users, null, outputFormat == "prettyjson" ? 4 : undefined);
+}
+
+if (outputfile){
+    let filename = "./out/" + outputfile;
+    let file = fs.createWriteStream(filename, {encoding: "utf-8"});
+
+    file.write(resultString);
+} else {
+    console.log(resultString);
 }
