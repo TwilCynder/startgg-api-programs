46067786b10e93bc1ecde3e60bdd515b73cf51ed
TwilCynder <twilcynder@gmail.com>
2024-06-18 18:38:23 +0200

    WIP mordernizing compareStandings

--- compareStandings.js
+++ compareStandings.js
@@ -2,6 +2,19 @@ import { client } from "./include/lib/client.js";
 import * as fs from 'fs'
 import { User } from "./include/user.js";
 import * as SC from "./include/computeStandingComparison.js";   
+import { ArgumentsManager } from "@twilcynder/arguments-parser"; 
+import { addInputParams, addOutputParams, doWeLog } from "./include/lib/paramConfig.js";
+
+let {slugs, outputFormat, outputfile, logdata, printdata, silent} = new ArgumentsManager()
+    .addParameter("IDsFilename", {}, false)
+    .addParameter("startDate", {type: "number"}, true)
+    .addParameter("endDate", {type: "number"}, true)
+    .apply(addOutputParams)
+    .enableHelpParameter()
+    .setMissingArgumentBehavior("Missing argument", 1, false)
+    .parseProcessArguments();
+
+let [logData_, silent_] = doWeLog(logdata, printdata, outputfile, silent);
 
 if (process.argv.length < 3 ){
     console.log("Usage : " + process.argv[0] + " " + process.argv[1] + " IDsListFilename [startDate [endDate]]");
