f208438b76d031dc7a41d960b38a9407ccacf564
TwilCynder <twilcynder@gmail.com>
2024-07-02 20:34:25 +0200

    downloadeventsstandings.js

---
+++ download/downloadEventsStandings.js
@@ -0,0 +1,35 @@
+
+
+import { EventListParser } from "../include/lib/computeEventList.js";
+import { ArgumentsManager } from "@twilcynder/arguments-parser"; 
+
+import { client } from "../include/lib/client.js";
+import { StartGGDelayQueryLimiter } from "../include/lib/queryLimiter.js";
+
+import { muteStdout, unmuteStdout } from "../include/lib/lib.js";
+import { addOutputParamsBasic, isSilent } from "../include/lib/paramConfig.js";
+import { outputJSON } from "../include/lib/util.js";
+import { getEventsResults } from "../include/getEventResults.js";
+
+let {events, outputfile, printdata, silent} = new ArgumentsManager()
+    .addCustomParser(new EventListParser, "events")
+    .apply(addOutputParamsBasic)
+    .enableHelpParameter()
+    .parseProcessArguments();
+
+printdata = printdata || !outputfile;
+let silent_ = isSilent(printdata, silent)
+
+if (silent_) muteStdout();
+
+console.log(events);
+
+let limiter = new StartGGDelayQueryLimiter();
+let data = await getEventsResults(client, events, undefined, limiter);
+limiter.stop();
+
+if (silent_){
+    unmuteStdout();
+}
+
+outputJSON(data, outputfile, printdata);
