4b2e1d09ad02a193a6e8f8841a3a2d40619630ef
TwilCynder <twilcynder@gmail.com>
2024-07-03 05:00:19 +0200

    deep_get_supports array indexes

--- include/lib/lib.js
+++ include/lib/lib.js
@@ -28,9 +28,26 @@ export async function readJSONAsync(filename){
         .then(buf => JSON.parse(buf))
 }
 
+/**
+ * 
+ * @param {{}} obj 
+ * @param {string} path 
+ * @param {*} def 
+ * @returns 
+ */
 export function deep_get(obj, path, def = null){
     //https://stackoverflow.com/a/8817473
-    for (var i=0, path=path.split('.'), len=path.length; i<len; i++){
+    path = path=path.split('.');
+    for (let i = 0; i < path.length; i++){
+        if (/^\d/.test(path[i])){
+            let n = parseInt(path[i]);
+            if (!isNaN){
+                path[i] = n;
+            }
+        }
+    }
+
+    for (var i=0, len=path.length; i<len; i++){
         obj = obj[path[i]];
         if (obj == undefined) return def;
     };
