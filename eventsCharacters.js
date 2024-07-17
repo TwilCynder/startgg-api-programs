179f2ac915c79fcff01b6686766315badcd6bd81
TwilCynder <twilcynder@gmail.com>
2024-07-17 19:00:00 +0200

    modernized events characters

--- eventsCharacters.js
+++ eventsCharacters.js
@@ -1,50 +1,67 @@
-import { parseArguments, OutputModeParser } from '@twilcynder/arguments-parser'
 import { EventListParser } from './include/lib/computeEventList.js'
-import { getCharsInEvents } from './include/getCharactersInEvent.js';
+import { getCharsInSets, getSetsCharsInEvents } from './include/getCharactersInEvent.js';
 import { client } from './include/lib/client.js';
 import { loadCharacterInfo } from './include/loadCharacterInfo.js';
-import fs from 'fs';
+import { addInputParams, addOutputParams, doWeLog } from './include/lib/paramConfig.js';
+import { output, readMultimodalInput } from './include/lib/util.js';
+import { StartGGDelayQueryLimiter } from './include/lib/queryLimiter.js';
+import { ArgumentsManager } from '@twilcynder/arguments-parser';
+import { muteStdout, unmuteStdout } from './include/lib/lib.js';
 
 try {
-    let [outputMode, list] = parseArguments(process.argv.slice(2), new OutputModeParser("stdout"), new EventListParser());
+    let {charactersInfoFilename, gameSlug, events, inputfile, stdinput, outputFormat, outputfile, logdata, printdata, silent} = new ArgumentsManager()
+        .addCustomParser(new EventListParser, "events")
+        .apply(addInputParams)
+        .apply(addOutputParams)
+        .addOption(["-f", "--characters-filename"], {
+            description: "Path to a json file containing character info. You need to specify either this or charactersInfoFilename.",
+            dest: "charactersInfoFilename"
+        })
+        .addOption(["-g", "--game-slug"], {
+            description: "Slug of the videogame to pull character info from. You need to specify either this or charactersInfoFilename.",
+            dest: "gameSlug"
+        })
+        .enableHelpParameter()
+        .parseProcessArguments();
+
+    let [logdata_, silent_] = doWeLog(logdata, printdata, outputfile, silent);
+
+    if (silent_) muteStdout();
+
+    if (!gameSlug && !charactersInfoFilename){
+        throw "Neither <charactersInfoFilename> or <gameSlug> were specified (using -s or -f respectively)"
+    }
+
+    let limiter = new StartGGDelayQueryLimiter();
+
+    let [data, charNames] = await Promise.all([
+        readMultimodalInput(inputfile, stdinput, getSetsCharsInEvents(client, events, limiter)),
+        loadCharacterInfo(charactersInfoFilename, client, limiter, gameSlug, true)
+    ])
+
+    limiter.stop();
 
 
-    let [charStats, charNames] = await Promise.all([
-        getCharsInEvents(client, list),
-        loadCharacterInfo("out/ssbu_chars.json", client, "game/ultimate", true)
-    ]);
-    
-    console.log("charsstat", charStats);
-    
+    let charStats = getCharsInSets(data);
+
     let result = [];
     for (let char in charStats){
         result.push({name: charNames[char], count: charStats[char]})
     }
     result.sort((a, b) => a.count - b.count);
     
-    console.log(result);
-
-    for (let char of result){
-        console.log(char.name, ":", char.count);
+    if (silent_) unmuteStdout();
+ 
+    if (logdata_){
+        for (let char of result){
+            console.log(char.name, ":", char.count);
+        }
     }
 
-    if (outputMode.file){
-        let filename = "./out/" + outputMode.file;
-        let file = fs.createWriteStream(filename, {encoding: "utf-8"});
-        
-        file.write(JSON.stringify(result));
-    }
-    
-    switch (outputMode.stdout){
-        case "log":
-            console.log(result);
-            break;
-        case "string": 
-            console.log(JSON.stringify(result));
-    }
+    output(outputFormat, outputfile, printdata, result, (result) => result.reduce((prev, current) => 
+        prev + current.name + "\t" + current.count + '\n'
+    ), "")
 } catch (e) {
-    console.error("SHIT WENT WRONG")
+    console.error("AN ERROR HAS OCCURED")
     console.error(e)
 }
-
-console.error("END");   
