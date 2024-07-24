553abd238c2b6f12e42a154a2a1ab16b1c3c009c
TwilCynder <twilcynder@gmail.com>
2024-07-24 02:00:00 +0200

    WIP added functions to process more character stats and moved all character stats functions to processCharacterStats.js

--- include/getCharactersInEvent.js
+++ include/getCharactersInEvent.js
@@ -1,12 +1,13 @@
 import { Query } from './lib/query.js';
 import { readSchema } from './lib/lib.js';
 import { getSetsInEvent, getSetsInEvents, reduceSetsInEvents } from './getSetsInEvents.js';
+import { getCharsInSets, processSets } from './processCharacterStats.js';
 
 const schema = readSchema(import.meta.url, "./GraphQLSchemas/EventSetsCharacter.txt");
 const query = new Query(schema, 3);
 
 query.log = {
-    query: params => `Fetching sets from event ${params.slug}, page ${params.page} (${params.perPage} entries per page) ...`,
+    query: params => `Fetching sets from event ${params.slug} with character info, page ${params.page} (${params.perPage} entries per page) ...`,
     error: params => `Request failed for event ${params.slug}, page ${params.page} (${params.perPage} entries per page) ...`
 }
 
@@ -18,36 +19,15 @@ export async function getSetsCharsInEvents(client, slugs, limiter){
     return await getSetsInEvents(client, query, slugs, limiter);
 }
 
-
-function updateCharsCount(chars, sets){
-    for (let set of sets){
-        if (!set.games) continue;
-        for (let game of set.games){
-            if (!game.selections) continue;
-            for (let selection of game.selections){
-                let char = selection.selectionValue;
-
-                if (!chars[char]) chars[char] = 0;
-                chars[char]++;
-            }
-        }
-    }
-    return chars;
+export async function getCharsInEvent(client, slug, limiter, updateFunction){
+    return getCharsInSets(await getSetsCharsInEvent(client, slug, limiter), updateFunction);
 }
 
-export function getCharsInSets(sets){
-    return updateCharsCount({}, sets);
-}
-
-export async function getCharsInEvent(client, slug, limiter){
-    return getCharsInSets(await getSetsCharsInEvent(client, slug, limiter));
-}
-
-export async function getCharsInEvents(client, slugs, limiter = null){
+export async function getCharsInEvents(client, slugs, limiter, updateFunction){
     let chars = await reduceSetsInEvents(client, query, slugs, (chars, sets) => {
         console.log("Reducing", chars);
-        if (!sets) return chars;    
-        return updateCharsCount(chars, sets);
+        if (!sets) return chars;
+        return processSets(chars, sets, updateFunction);
     }, {}, limiter)
     console.log(chars);
 
---
+++ include/processCharacterStats.js
@@ -0,0 +1,99 @@
+import { deep_get } from "./lib/lib";
+
+function updateCharsGamesCount(chars, set){
+    for (let game of set.games){
+        if (!game.selections) continue;
+        for (let selection of game.selections){
+            let char = selection.selectionValue;
+
+            if (!chars[char]) chars[char] = 0;
+            chars[char]++;
+        }
+    }
+    return chars;
+}
+
+function updateCharsGamesSetsCount(chars, set){
+    let seenChars = [];
+    for (let game of set.games){
+        if (!game.selections) continue;
+        for (let selection of game.selections){
+            let char = selection.selectionValue;
+
+            let charObj = chars[char];
+            if (!charObj){
+                charObj = {games: 0, sets: 0};
+                chars[char] = charObj;
+            }
+
+            if (!seenChars.includes(char)){
+                seenChars.push(char);
+                charObj.sets++;
+            }
+            charObj.games++;
+        }
+    }
+    return chars;
+}
+
+function updateCharsGamesCountPlayers(chars, set){
+    for (let game of set.games){
+        if (!game.selections) continue;
+        //for (let selection of game.selections){
+		for (let i = 0; i < game.slots.length; i++){
+			let slot = set.slots[i];
+			let selection = game.selections[i];
+
+            let char = selection.selectionValue;
+
+            let charObj = chars[char];
+            if (!charObj){
+                charObj = {games: 0, players: {}};
+                chars[char] = charObj;
+            }
+			charObj.games++;
+
+			let participants = deep_get(slot, "entrant.particiapnts");
+			if (!participants || !participants[0]) continue;
+			for (let participant of participants){
+				let player = participant.player;
+				if (!player) continue;
+				let playerObj = charObj.players[player.id];
+				if (!playerObj){
+					playerObj = {id: player.id, name: player.gamerTag, games: 0};
+					charObj.players[player.id] = playerObj;
+				}
+				playerObj.games++;
+			}
+
+        }
+    }
+    return chars;
+}
+
+function updateCharsGamesSetsCountPlayers(chars, set){
+
+}
+
+/**
+ * Returns the update function that matches the requirements described by the arguments
+ * @param {boolean} sets true for an update function that processes sets
+ * @param {boolean} players true for an update function that processes individual player stats
+ */
+export function getUpdateFunction(sets, players){
+	return sets ?
+		(players ? updateCharsGamesSetsCountPlayers : updateCharsGamesSetsCount) :
+		(players ? updateCharsGamesCountPlayers : updateCharsGamesCount)
+}
+
+function processSets(chars, sets, updateFunction = updateCharsGamesCount){
+    for (let set of sets){
+        if (!set.games) continue;
+        updateFunction(chars, set);
+    }
+    return chars;
+}
+
+export function getCharsStatsInSets(sets, updateFunction){
+    return processSets({}, sets, updateFunction);
+}
