c6e2c85df3ffbce0945892ed69a00ef1fb2396cf
TwilCynder <twilcynder@gmail.com>
2024-09-28 15:22:44 +0200

    "modernized" entrantsCount

--- entrantsCount.js
+++ entrantsCount.js
@@ -1,19 +1,14 @@
 import fs from "fs";
 import { client } from "./include/lib/client.js";
 import { getEntrantsCountOverLeague } from "./include/getEntrantsCount.js";
-import { computeEventList } from "./include/lib/computeEventList.js";
+import { computeEventList, EventListParser } from "./include/lib/computeEventList.js";
+import { ArgumentsManager } from "@twilcynder/arguments-parser";
 
-if (process.argv.length < 3 ){
-    console.log("Usage : " + process.argv[0] + " " + process.argv[1] + " [-m min_attendance] {-f listFilename | -s template min max | slugs ...} ");
-    process.exit();
-}
-
-
-let list = computeEventList(process.argv.slice(2))
-if (!list) {
-    console.log("Usage : " + process.argv[0] + " " + process.argv[1] + " [-m min_attendance] {-f listFilename | -s template min max | slugs ...} ");
-    process.exit();
-}
+let {list, silent} = new ArgumentsManager()
+    .addSwitch(["-s", "--silent"])
+    .addCustomParser(new EventListParser, "list")
+    .enableHelpParameter()
+    .parseProcessArguments();
 
 let count = await getEntrantsCountOverLeague(client, list);
 
