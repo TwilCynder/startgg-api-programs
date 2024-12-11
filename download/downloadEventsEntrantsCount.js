86a63f49452c2abc93c7e93654c87e10a3790c05
TwilCynder <twilcynder@mail.com>
2024-12-11 18:08:48 +0100

    downloadeventsentrantscount

---
+++ download/downloadEventsEntrantsCount.js
@@ -0,0 +1,34 @@
+
+
+import { EventListParser } from "../include/lib/computeEventList.js";
+import { ArgumentsManager } from "@twilcynder/arguments-parser"; 
+
+import { client } from "../include/lib/client.js";
+import { StartGGDelayQueryLimiter } from "../include/lib/queryLimiter.js";
+
+import { muteStdout, unmuteStdout } from "../include/lib/jsUtil.js";
+import { addOutputParamsBasic, isSilent } from "../include/lib/paramConfig.js";
+import { outputJSON } from "../include/lib/util.js";
+import { getEntrantsCountOverLeague } from "../include/getEntrantsCount.js";
+
+let {events, outputfile, printdata, silent, prettyjson} = new ArgumentsManager()
+    .addCustomParser(new EventListParser, "events")
+    .apply(addOutputParamsBasic)
+    .addSwitch(["-r", "--readable-json"], {description: "Makes the JSON output human-readable", dest: "prettyjson"})
+    .enableHelpParameter()
+    .parseProcessArguments();
+
+printdata = printdata || !outputfile;
+let silent_ = isSilent(printdata, silent)
+
+if (silent_) muteStdout();
+
+let limiter = new StartGGDelayQueryLimiter();
+let data = await g
+limiter.stop();
+
+if (silent_){
+    unmuteStdout();
+}
+
+outputJSON(data, outputfile, printdata, prettyjson);
