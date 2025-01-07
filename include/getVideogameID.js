eb9fd14506e067c3b07c4ea82848fda01c94fc3b
TwilCynder <twilcynder@gmail.com>
2025-01-07 14:52:34 +0100

    getVideogameID.txt

---
+++ include/getVideogameID.js
@@ -0,0 +1,17 @@
+import { Query } from "./lib/query.js";
+import { readSchema } from './lib/util.js';
+import { deep_get } from './lib/jsUtil.js';
+
+let schema = readSchema(import.meta.url, "./GraphQLSchemas/VideogameID.txt");
+
+let query = new Query(schema, 2)
+query.log = {
+    query: (params) => "Fetching ID for videogame " + params.slug,
+    error: (params) => "Couldn't fetch ID for videogame " + params.slug
+}
+
+export async function getVideogameID(client, slug, limiter){
+    let result = await query.execute(client, {slug}, limiter);
+    console.log("Fetched ID for videogame", slug);
+    return deep_get(result, "videogame.id");
+}
