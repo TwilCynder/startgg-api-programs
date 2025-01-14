4fbc941fb624322eb2ad3ca6e5205c1c5fd7323b
TwilCynder <twilcynder@gmail.com>
2025-01-14 17:26:08 +0100

    add blacklist mode to filterEvents.js

--- process/filterEvents.js
+++ process/filterEvents.js
@@ -5,9 +5,11 @@ import { outputJSON, readInputData } from "../include/lib/util.js";
 import { filterEvents } from "../include/filterEvents.js";
 import { muteStdout, unmuteStdout } from "../include/lib/jsUtil.js";
 
-let {inputfile, stdinput, eventSlugs, eventsFilenames, exclude_expression, filter, outputfile, printdata, silent, prettyjson} = new ArgumentsManager() 
+let {inputfile, stdinput, eventSlugs, eventsFilenames, exclude_expression, filter, outputfile, printdata, silent, prettyjson, blacklistMode} = new ArgumentsManager() 
+    .setAbstract("Applies various filters to an array of events (with or without standings). See options to see all awailable filters. Keep in mind that if any events is specified, only these events will be kept ; -B reverses this.")
     .apply(addInputParams)
     .apply(addEventParsers)
+    .addSwitch(["-B", "--blacklist-mode"], {dest: "blacklistMode", description: "Treat events as blacklist instead of whitelist"})
     .apply(addEventNameFilterParams)
     .apply(addOutputParamsJSON)
     .enableHelpParameter()
@@ -25,12 +27,10 @@ let [data, events] = await Promise.all([
 if (events && events.length){
     data = data.filter(event => {
         for (let slug of events){
-            if (slug == event.slug) return true;
+            if (slug == event.slug) return !blacklistMode;
         }
-        return false;
-    });
-    
-    console.log(data);
+        return blacklistMode;
+    });    
 }
 
 data = filterEvents(data, exclude_expression, filter);
