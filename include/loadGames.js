281a669b8333cf5af59f6de07a1a5d3d0f88af91
TwilCynder <twilcynder@gmail.com>
2026-01-17 22:51:51 +0100

    rework loadGames, add processGameListString

--- include/loadGames.js
+++ include/loadGames.js
@@ -1,20 +1,35 @@
 import { getVideogameID } from "./getVideogameID.js";
-import { extractSlug } from "startgg-helper";
+import { extractSlug, TimedQuerySemaphore } from "startgg-helper";
 
+/**
+ * Returns an array of game IDs from a comma-separated list of game ids or game slugs
+ * @param {GraphQLClient} client 
+ * @param {string?} string 
+ * @param {TimedQuerySemaphore} limiter 
+ */
+export function processGameListString(client, string, limiter){
+    return string ? loadGames(client, string.split(","), limiter) : string;
+}
+
+/**
+ * Loads the numerical IDs for a list of game slugs or IDs.
+ * @param {GraphQLClient} client 
+ * @param {[number | string]} games 
+ * @param {TimedQuerySemaphore} limiter 
+ * @returns 
+ */
 export async function loadGames(client, games, limiter){
-    let gamesID = null;
-    if (games){
-        console.log("Fetching IDs for games", games);
-        gamesID = await Promise.all(games.split(",").map(word => {
-            word = word.trim();
-            if (!word.includes("game/")) word = "game/" + word;
-            let id = parseInt(word);
-            if (!id){ //assuming it was a slug
-                return getVideogameID(client, extractSlug(word), limiter);
+    return await Promise.all(games.map(game => {
+        if (typeof game == "number"){
+            return game;
+        } else {
+            const id = parseInt(game);
+            if (!id){ //assume it was a slug
+                if (!game.includes("game/")) game = "game/"+game;
+                return getVideogameID(client, extractSlug(game), limiter);
             } else {
                 return id;
             }
-        }))
-    }
-    return gamesID;
+        }
+    }))
 }
