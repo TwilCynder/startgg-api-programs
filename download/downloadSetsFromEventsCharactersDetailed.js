5824377e5399d041c505e64d8a85dd2f5de0f72b
TwilCynder <twilcynder@gmail.com>
2026-02-26 22:23:08 +0100

    WIP modernize download scripts (basic, chars, charsDetailed)

--- download/downloadSetsFromEventsCharactersDetailed.js
+++ download/downloadSetsFromEventsCharactersDetailed.js
@@ -6,15 +6,18 @@ import { StartGGDelayQueryLimiter } from "startgg-helper";
 
 import { muteStdout, unmuteStdout } from "../include/lib/fileUtil.js";
 import { addInputParams, addOutputParamsJSON, isSilent } from "../include/lib/paramConfig.js";
-import { outputJSON, tryReadJSONInput } from "../include/lib/util.js";
-import { QueriesProgressManager } from "../include/progressSaver.js";
-import { formatM } from "../include/lib/consoleUtil.js";
+import { outputJSON, tryReadJSONArray } from "../include/lib/util.js";
+import { queriesProgressManager } from "../include/progressSaver.js";
 import { getSetsCharsDetailedInEvents, getSetsCharsDetailedInEventsFromObjects, getSetsCharsDetailedInEventsHashMap, getSetsCharsDetailedInEventsSeparated } from "../include/getCharactersInEventsDetailed.js";
+import { formatM } from "../include/lib/consoleUtil.js";
 
-let {eventSlugs, eventsFilenames, inputfile, mode, outputfile, printdata, silent, prettyjson} = new ArgumentsManager()
+let {eventSlugs, eventsFilenames, inputfile, mode, outputfile, printdata, silent, prettyjson, fragmentOutput, cache, cache_frequency} = new ArgumentsManager()
+    .setParameters({guessLowDashes: true})
     .apply(addEventParsers)
     .apply(addInputParams)
-    .addSwitch(["-m", "--mode"], {description: `Changes the way sets are organized in the output. Can be either : "${formatM("flat", "underline", "bold")}" (Outputs a single array containing all sets of all events) ; "${formatM("hashmap", "underline", "bold")}" (Outputs a hashmap with an array of sets for each event, with the even slug as key) ; "${formatM("objects", "underline", "bold")}" (Outputs an array of event objects, with two properties : slug and sets) ; "${formatM("arrays", "underline", "bold")}" (default) (Outputs an array of arrays)`})
+    .addOption(["-c", "--cache"], {description: "File to use as cache for queries (useful is the program crashes during execution)"})
+    .addOption(["--cache-frequency"], {description: "How often does the program write to cache (in number of queries)"})
+    .addOption(["-m", "--mode"], {description: `Changes the way sets are organized in the output. Can be either : "${formatM("flat", "underline", "bold")}" (Outputs a single array containing all sets of all events) ; "${formatM("hashmap", "underline", "bold")}" (Outputs a hashmap with an array of sets for each event, with the even slug as key) ; "${formatM("objects", "underline", "bold")}" (Outputs an array of event objects, with two properties : slug and sets) ; "${formatM("arrays", "underline", "bold")}" (default) (Outputs an array of arrays)`})
     .apply(addOutputParamsJSON)
     .enableHelpParameter()
     .parseProcessArguments();
@@ -24,16 +27,16 @@ let silent_ = isSilent(printdata, silent)
 
 if (silent_) muteStdout();
 
-let [events, eventObjects] = await Promise.all([readEventLists(eventSlugs, eventsFilenames), tryReadJSONInput(inputfile)]);
+let [events, eventObjects] = await Promise.all([readEventLists(eventSlugs, eventsFilenames), tryReadJSONArray(inputfile)]);
 
 let limiter = new StartGGDelayQueryLimiter();
-let progressManager = new QueriesProgressManager("./out/testProgress2.json", {writeThreshold: 100});
+let progressManager = cache ? await queriesProgressManager(cache, {writeThreshold: cache_frequency ?? 100}) : null;
 
 let data;
 mode = mode ?? (eventObjects.length > 0 && !events.length ? "objects" : "arrays");
 if (mode.startsWith("o")){
     eventObjects = eventObjects.concat(events.map(slug => ({slug})));
-    data = await getSetsCharsDetailedInEventsFromObjects(client, events, limiter, progressManager);
+    data = await getSetsCharsDetailedInEventsFromObjects(client, eventObjects, limiter, progressManager);
 } else {
     events = events.concat(eventObjects.map(event => event.slug).filter(v=>!!v));
     if (mode.startsWith("a")){
@@ -50,4 +53,4 @@ if (silent_){
     unmuteStdout();
 }
 
-outputJSON(data, outputfile, printdata, prettyjson);
+outputJSON(data, outputfile, printdata, prettyjson, fragmentOutput);
